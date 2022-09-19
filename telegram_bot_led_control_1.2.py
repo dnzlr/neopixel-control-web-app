9de1ef9ff4e1731eb5a954a8529671d01d8b0fc3
dnzlr <0x6371@protonmail.com>
2022-09-19 12:09:35 +0200

    update font size on and off buttons, add token handling

--- telegram_bot_led_control_1.2.py
+++ telegram_bot_led_control_1.2.py
@@ -10,89 +10,65 @@ import time
 import random
 from multiprocessing import Process
 
-bot = telebot.TeleBot("yourToken")
+# create a file called ".telegramToken.txt" and store your
+# telegram token inside
+# TODO(Y):explain how to retrieve the token
+# read token from .telegramToken.txt file
+f = open('.telegramToken.txt', 'r')
+telegramToken = f.read().strip()
+f.close()
+bot = telebot.TeleBot(str(telegramToken))
 
-p1 = Process(target=yl.rainbowCycle, args=(yl.pixels,))
-#p1 = Process(target=rainbowCycle, args=strip)
-
+# TODO(Y): Problem: Not able to effectively turn off this function yet!
 @bot.message_handler(commands=['bunt', 'rb', 'rainbow', 'colors'])
 def rainbow(message):
     glo.isAlive = True
-    #p1 = Process(target=yl.rainbowCycle, args=(yl.pixels,mybool==True))
-    yl.rainbowCycle(yl.pixels)
+    #p1 = Process(target=yl.rainbowCycle, args=(yl.ledObject,mybool==True))
+    yl.rainbowCycle(yl.ledObject)
     #p1.start()
     #rainbowCycle(strip)
 
 @bot.message_handler(commands=['off', 'k', 'kill'])
 def off(message):
     yl.wheelOff()
-    print("KILL PROCESS")
-    yl.colorWipe(yl.pixels, Color(0,0,0), 10)
+    #yl.colorWipe(yl.ledObject, Color(0,0,0), 10)
     bot.reply_to(message, "Light off")
 
 @bot.message_handler(commands=['onw', 'w', 'white'])
 def on(message):
-    if p1.is_alive():
-        p1.terminate()
-        p1.kill()
-    yl.ypsCol(yl.pixels, yl.WHITE)
+    yl.ypsCol(yl.ledObject, yl.WHITE)
 
 @bot.message_handler(commands=['on', 'ww', 'warmwhite'])
 def warmWhite(message):
-    if p1.is_alive():
-        p1.terminate()
-        p1.kill()
-    yl.ypsCol(yl.pixels, yl.WARMWHITE)
+    yl.ypsCol(yl.ledObject, yl.WARMWHITE)
 
 @bot.message_handler(commands=['brightyellow', 'c', 'citrus'])
 def citrus(message):
-    if p1.is_alive():
-        p1.terminate()
-        p1.kill()
-    yl.ypsCol(yl.pixels, yl.YELLOW)
+    yl.ypsCol(yl.ledObject, yl.YELLOW)
 
 @bot.message_handler(commands=['green', 'g', 'weed', 'kush'])
 def green(message):
-    if p1.is_alive():
-        p1.terminate()
-        p1.kill()
-    yl.ypsCol(yl.pixels, yl.GREEN)
+    yl.ypsCol(yl.ledObject, yl.GREEN)
 
 @bot.message_handler(commands=['red', 'r', 'sex'])
 def red(message):
-    if p1.is_alive():
-        p1.terminate()
-        p1.kill()
-    yl.ypsCol(yl.pixels, yl.RED)
+    yl.ypsCol(yl.ledObject, yl.RED)
 
 @bot.message_handler(commands=['blue', 'b', 'sad'])
 def blue(message):
-    if p1.is_alive():
-        p1.terminate()
-        p1.kill()
-    yl.ypsCol(yl.pixels, yl.BLUE)
+    yl.ypsCol(yl.ledObject, yl.BLUE)
 
 @bot.message_handler(commands=['yellow', 'y'])
 def yellow(message):
-    if p1.is_alive():
-        p1.terminate()
-        p1.kill()
-    yl.ypsCol(yl.pixels, yl.ORANGE)
+    yl.ypsCol(yl.ledObject, yl.ORANGE)
 
 @bot.message_handler(commands=['pink', 'p'])
 def pink(message):
-    if p1.is_alive():
-        p1.terminate()
-        p1.kill()
-    yl.ypsCol(yl.pixels, yl.PINK)
+    yl.ypsCol(yl.ledObject, yl.PINK)
 
 #@bot.message_handler(commands=['off', '0'])
 #def off(message):
-#    if p1.is_alive():
-#        p1.terminate()
-#        p1.kill()
 #    print("STOP THREADS = true")
 #    yl.ypsCol(yl.OFF)
-
 
 bot.infinity_polling()
